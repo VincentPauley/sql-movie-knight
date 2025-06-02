f07f843f07eb467816a8cee7189315443d51585f
Vinnie Pauley <vinniepauley@gmail.com>
2025-06-02 14:02:36 -0500

    fastapi performing database reads

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+# Movie Knight
+
+This is a full-stack app that I use with my wife to keep track of movies that we like/dislike. It helps us figure out what we want to watch next
+
+## Getting Started
+
+### System Requirements
+
+- Docker
+
+### Setup the app locally
+
+clone this repo and create a `.env` at the root of the project based on `.env.example`. Be sure to set passwords in here.
+
+From there just run the docker compose and all of the app services will be started and you can begin using the app.
+
+```bash
+docker-compose up
+```
+
 - tags table
 - tag junctions table
 
@@ -5,8 +25,4 @@
 
 Up Next:
 
-update junction table json to include the ids from the movie table... this
-will include reading direct from sql and performing title match now that there
-have been some corrections.
-
-get genre ids into a junction_table json file
+Need to have an environment file to hold the DB info and pass it to the API service as well.
--- app/main.py
+++ app/main.py
@@ -1,8 +1,35 @@
+import os
+
+import mysql.connector
 from fastapi import FastAPI
 
 app = FastAPI()
 
+MYSQL_HOST = os.getenv("MYSQL_HOST", "db")  # Use the container name as the hostname
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "default_database")
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "password")
 
-@app.get("/")
+
+@app.get("/10-random-movies")
 def read_root():
-    return {"message": "Hello World from api!"}
+    try:
+        conn = mysql.connector.connect(
+            host=MYSQL_HOST,
+            database=MYSQL_DATABASE,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+        )
+        cursor = conn.cursor()
+
+        # Example query: Fetch all rows from a table named 'movies'
+        cursor.execute("SELECT * FROM movies ORDER BY RAND() LIMIT 10")
+        movies = cursor.fetchall()
+
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+        return {"message": "here are 10 random movies", "movies": movies}
+    except mysql.connector.Error as err:
+        return {"error": f"error happened: {err}"}
