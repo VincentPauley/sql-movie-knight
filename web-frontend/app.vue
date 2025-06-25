feef5ba38ac0cf6e5131d1d91147028920be81fc
Vinnie Pauley <vinniepauley@gmail.com>
2025-06-24 23:12:29 -0500

    feat: genre separation

--- web-frontend/app.vue
+++ web-frontend/app.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
     <h1>Movie Knight</h1>
+
+    <h3>Main Genres {{mainGenres.length}}</h3>
     <ul>
-      <li v-for="genre in genres" :key="genre.id">
-        {{genre.name}}
+      <li v-for="mainGenre in mainGenres" :key="mainGenre.id" >
+        {{mainGenre.name}}
       </li>
     </ul>
+    <h3>Sub-Genres {{subGenres.length}}</h3>
+    <ul>
+      <li v-for="subGenre in subGenres" :key="subGenre.id" >
+        {{subGenre.name}}
+      </li>
+    </ul> 
+    <h3>Tag-Genres {{tagGenres.length}}</h3>
+    <ul>
+      <li v-for="tagGenre in tagGenres" :key="tagGenre.id" >
+        {{tagGenre.name}}
+      </li>
+    </ul> 
   </div>
 </template>
 
@@ -16,6 +30,17 @@ export default {
       genresFetched: false,
       genres: []
     }
+  },
+  computed: {
+    mainGenres() {
+      return this.genres.filter(genre => genre.level === 1)
+    },
+    subGenres() {
+      return this.genres.filter(genre => genre.level === 2)
+    },
+    tagGenres() {
+      return this.genres.filter(genre => genre.level === 3)
+    },
   },
   mounted() {
     if (!this.genresFetched) {
