a7a47889f3d943b4ff79895c05fffeefc9e70c51
Vinnie Pauley <vinniepauley@gmail.com>
2025-06-24 20:18:20 -0500

    feat: progress on fe setup

--- web-fe/src/App.tsx
+++ web-fe/src/App.tsx
@@ -1,6 +1,25 @@
 import './App.css'
 
+import { useEffect } from 'react'
+
 function App() {
+
+  useEffect(() => {
+    fetch('rest_api/movies?genres=88c18939-389d-485c-8b25-9150c7a4b321') // Use the service name as the hostname
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log('Parsed JSON:', data);
+    })
+    .catch((error) => {
+      console.error('FETCH ERROR:', error);
+    });
+  }, [])
+
   return (
     <>
       <div>
